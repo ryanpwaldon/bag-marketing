0c8a08025ae62e46c2551bdded3a0093fa8e0f69
Ryan Waldon <ryanpwaldon@gmail.com>
2020-11-28 19:56:39 +1100

    feat: updates

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,44 +1,44 @@
 <template>
-  <div class="grid gap-20">
+  <div class="grid gap-20 text-center">
     <Header />
-    <div class="flex flex-col items-center px-5 text-center">
+    <div class="flex flex-col items-center px-5">
       <h1 class="text-3xl font-semibold leading-tight">Bag, a shopping cart<br />for sales growth</h1>
       <p class="max-w-xs mt-4 text-gray-500">
         Impress your customers with a smooth checkout experience and grow your sales at the same time.
       </p>
-      <Button text="Get early access" class="mt-4" />
+      <Button text="Get early access" class="mt-5" />
       <Browser class="w-full mt-16" />
     </div>
-    <div class="px-5">
+    <div class="flex flex-col items-center px-5">
       <Bolt class="text-blue-700" />
-      <h3 class="mt-3 text-2xl font-semibold">A new way to<br />grow your sales</h3>
-      <p class="mt-2 text-base text-gray-500">
-        Sell more with smart cross sells. They’re highly targeted offers that dynamically change depending on the items in your customer’s cart.
+      <h3 class="mt-4 text-2xl font-semibold">A new way to grow sales</h3>
+      <p class="mt-4 text-base text-gray-500">
+        Sell more with smart cross sells. They’re highly targeted offers that change depending on the items in each customers' cart.
       </p>
-      <Quote class="mt-6">You're buying a tape dispenser...<br />Need some tape?</Quote>
-      <Figure1 class="w-full mt-6" />
+      <Figure1 class="w-full mt-8" />
+      <Quote class="inline-flex mt-8 text-left">You're buying a tape dispenser...<br />Need some tape?</Quote>
     </div>
-    <div class="px-5">
+    <div class="flex flex-col items-center px-5">
       <Sparkles class="text-blue-700" />
-      <h3 class="mt-3 text-2xl font-semibold">Leave a great<br />impression</h3>
-      <p class="mt-2 text-base text-gray-500">
+      <h3 class="mt-4 -mx-2 text-2xl font-semibold">Your customers will thank you</h3>
+      <p class="mt-4 text-base text-gray-500">
         Give your customers a smooth checkout experience. Our variant editor makes it easy to review items and make changes.
       </p>
-      <Figure2 class="w-full mt-6" />
+      <Figure2 class="w-full mt-8" />
     </div>
-    <div class="px-5 text-center">
-      <h3 class="text-2xl font-semibold">Cart manager</h3>
-      <p class="mt-2 text-base text-gray-500">
+    <div class="flex flex-col items-center px-5">
+      <Chart class="text-blue-700" />
+      <h3 class="mt-4 text-2xl font-semibold">Cart manager</h3>
+      <p class="mt-4 text-base text-gray-500">
         Create cross sells and track their performance in our easy-to-use Shopify embedded app.
       </p>
-      <img class="w-full mt-6 rounded-md shadow-md" src="@/assets/img/admin.png" alt="Cart manager" />
+      <img class="w-full mt-8 rounded-md shadow-md" src="@/assets/img/admin.png" alt="Cart manager" />
     </div>
-    <div class="px-5 text-center">
-      <h3 class="text-2xl font-semibold">Get free access</h3>
-      <p class="mt-2 text-base text-gray-500">
-        For a limited time, we're providing free access to early users.
-      </p>
-      <EmailForm class="mt-6" />
+    <div class="flex flex-col items-center px-5">
+      <Bag />
+      <h3 class="mt-4 text-2xl font-semibold">Get early access</h3>
+      <p class="mt-4 text-base text-gray-500">Help us build a tool that enhances<br />your Shopify experience.</p>
+      <Button class="mt-5 mb-24" text="Get early access" />
     </div>
   </div>
 </template>
@@ -52,8 +52,9 @@ import Quote from '@/components/Quote/Quote.vue'
 import Figure1 from '@/components/Figure1/Figure1.vue'
 import Sparkles from '@/icons/Sparkles/Sparkles.vue'
 import Figure2 from '@/components/Figure2/Figure2.vue'
-import EmailForm from '@/components/EmailForm/EmailForm.vue'
 import Button from '@/components/Button/Button.vue'
+import Chart from '@/icons/Chart/Chart.vue'
+import Bag from '@/icons/Bag/Bag.vue'
 export default defineComponent({
   components: {
     Header,
@@ -63,8 +64,9 @@ export default defineComponent({
     Figure1,
     Sparkles,
     Figure2,
-    EmailForm,
-    Button
+    Button,
+    Chart,
+    Bag
   }
 })
 </script>
