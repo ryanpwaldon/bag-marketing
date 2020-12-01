2162ec7c9baa962a52e3344e477d971f9c642ef4
Ryan Waldon <ryanpwaldon@gmail.com>
2020-12-01 16:06:00 +1100

    feat: copy

--- src/views/Join/Join.vue
+++ src/views/Join/Join.vue
@@ -4,15 +4,15 @@
       <Logo />
     </router-link>
     <div class="w-full max-w-full bg-white rounded shadow sm:w-96">
-      <div class="grid gap-1 p-6 sm:py-6 sm:px-8">
-        <h3 class="text-lg font-medium">Get early access</h3>
-        <p class="text-sm text-gray-500">Streamline your checkout, grow your sales.</p>
+      <div class="grid gap-1 p-6 text-center sm:py-6 sm:px-8">
+        <h3 class="text-lg font-medium">Join early access</h3>
+        <p class="text-sm text-gray-500">Ready to streamline your checkout<br />and grow your sales?</p>
       </div>
       <div class="w-full h-px bg-gray-200" />
       <form class="grid gap-6 p-6 sm:p-8" @submit="handleSubmit">
         <InputText name="Email" placeholder="Your email" v-model="fields.email.value.value" :error="fields.email.error.value" />
         <InputText name="Shop URL" placeholder="Your shop website" v-model="fields.shopUrl.value.value" :error="fields.shopUrl.error.value" />
-        <Button type="submit" size="md" text="Register" />
+        <Button type="submit" size="md" text="Get early access" />
       </form>
     </div>
     <p class="text-sm text-gray-500">Made for Shopify</p>
