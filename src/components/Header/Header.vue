f9115ebfad755622da051117f4855f8f92858285
Ryan Waldon <ryanpwaldon@gmail.com>
2021-05-11 14:06:39 +0930

    feat: update favicon / logo

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-between w-full max-w-4xl py-8 m-auto">
-    <Logo class="w-auto h-6" />
+    <Logo class="w-auto h-7" />
     <a href="http://apps.shopify.com/bag" target="_blank" class="text-sm font-medium text-gray-900 border-b border-transparent hover:border-gray-900">
       View on Shopify →
     </a>
